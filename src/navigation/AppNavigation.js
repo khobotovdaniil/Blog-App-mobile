203abafad9b21fa828d7c185634c14851e3f0ca0
Daniil_Khobotov <dinich44@gmail.com>
2023-10-18 16:56:35 +0400

    complete bottom tabs

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,30 +1,45 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from 'react-native-paper';
 
 import { THEME } from "../theme";
 import { BookedStackScreen } from './stackScreens/BookedStackScreen'
 import { MainStackScreen } from "./stackScreens/MainStackScreen";
+import { Platform } from "react-native";
 
-const BottomTab = createBottomTabNavigator();
+const BottomTab = Platform.OS === 'android' ? createMaterialBottomTabNavigator() : createBottomTabNavigator();
 
 export const AppNavigation = () => {
+  const theme = useTheme();
+  theme.colors.secondaryContainer = "transperent"
   return (
     <NavigationContainer>
       <BottomTab.Navigator
+        activeColor={Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR}
+        inactiveColor='#9fa7b6'
+        barStyle={{
+          backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+          height: 70,
+        }}
+        shifting={true}
+        tabBarOptions={{
+          activeBackgroundColor: 'transparent'
+        }}
         screenOptions={({ route }) => ({
           tabBarIcon: ({ color }) => {
             let iconName = route.name === 'All' ? 'ios-albums' : 'ios-star'
 
             return <Ionicons name={iconName} size={25} color={color} />
           },
-          headerShown: false,
-          tabBarActiveTintColor: THEME.MAIN_COLOR
+          tabBarLabel: route.name === 'All' ? 'Все' : 'Избранное',
+          headerShown: false
         })}
       >
-        <BottomTab.Screen name="Все" component={MainStackScreen} />
-        <BottomTab.Screen name="Избранное" component={BookedStackScreen} />
+        <BottomTab.Screen name="All" component={MainStackScreen} />
+        <BottomTab.Screen name="Marked" component={BookedStackScreen} />
       </BottomTab.Navigator>
     </NavigationContainer>
   )
